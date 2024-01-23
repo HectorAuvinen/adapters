70fa07c3a8f3d06c5706a968ba52a0b0821256c3
HectorAuvinen <56079098+HectorAuvinen@users.noreply.github.com>
2024-01-23 17:43:18 +0100

    change metric function

--- examples/pytorch/text-classification/run_glue.py
+++ examples/pytorch/text-classification/run_glue.py
@@ -497,6 +497,7 @@ def main():
 
     # You can define your custom compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
     # predictions and label_ids field) and has to return a dictionary string to float.
+    """    
     def compute_metrics(p: EvalPrediction):
         preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
         preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
@@ -509,6 +510,10 @@ def main():
             return {"mse": ((preds - p.label_ids) ** 2).mean().item()}
         else:
             return {"accuracy": (preds == p.label_ids).astype(np.float32).mean().item()}
+    """
+    def compute_metrics(p: EvalPrediction):
+        preds = np.argmax(p.predictions, axis=1)
+        return {"acc": (preds == p.label_ids).mean()}
 
     # Data collator will default to DataCollatorWithPadding when the tokenizer is passed to Trainer, so we change it if
     # we already did the padding.
